4e483918f3c695a20b35876a19a8c01869df341b
abdelrahman <rain.forest5023@gmail.com>
2025-12-18 19:01:47 +0200

    refactor task widget ui

--- lib/widgets/task_widget.dart
+++ lib/widgets/task_widget.dart
@@ -1,4 +1,5 @@
-import 'package:bexel_task/model/task_model.dart';
+import 'package:bexel_task/data/model/task_model.dart';
+import 'package:bexel_task/widgets/extensions/date_extensions.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -10,22 +11,40 @@ class TaskWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [Text(task.title), Chip(label: Text(task.status))],
-          ),
-          Expanded(child: Text(task.description)),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(task.title),
-              Text(task.createdAt.toIso8601String())
-            ],
-          )
-        ],
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(task.title),
+                Chip(
+                  label: Text(
+                    task.status,
+                    style: TextStyle(fontSize: 10),
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Text(task.description),
+            const SizedBox(
+              height: 24,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(task.type),
+                Text(task.createdAt.formattedDateTime)
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
