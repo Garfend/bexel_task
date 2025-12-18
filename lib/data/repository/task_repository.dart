5e1bac28ff27ca61de27794c313c24befe9e67d4
abdelrahman <rain.forest5023@gmail.com>
2025-12-18 19:59:13 +0200

    add filteration

--- lib/data/repository/task_repository.dart
+++ lib/data/repository/task_repository.dart
@@ -2,16 +2,49 @@ import 'package:bexel_task/data/datasource/task_datasource.dart';
 import 'package:bexel_task/data/model/task_model.dart';
 
 abstract class TaskRepository {
-  Future<List<TaskModel>> loadTask();
+  Future<List<TaskModel>> loadTasks();
+  Future<List<String>> loadTaskTypes();
 }
 
 class TaskRepositoryImp extends TaskRepository {
   final TaskDatasource taskDatasource;
 
-  TaskRepositoryImp(this.taskDatasource);
+  TaskRepositoryImp(this.taskDatasource, this.localDb);
 
   @override
-  Future<List<TaskModel>> loadTask() async {
-    return await taskDatasource.loadTasks();
+  Future<List<TaskModel>> loadTasks() async {
+    return await localDb.fetchAll();
+  }
+
+  @override
+  Future<List<String>> loadTaskTypes() async {
+    final tasks = await loadTasks();
+    final types = tasks
+        .map((task) => task.type)
+        .toSet()
+        .toList()
+      ..sort();
+    return types;
+  }
+
+  @override
+  Future<void> importFromAssets() async {
+    final tasks = await taskDatasource.loadTasks();
+    await localDb.upsertTasks(tasks, clearBeforeInsert: true);
+  }
+
+  @override
+  Future<void> addTask(TaskModel task) async {
+    await localDb.insertTask(task);
+  }
+
+  @override
+  Future<void> updateTask(TaskModel task) async {
+    await localDb.updateTask(task);
+  }
+
+  @override
+  Future<void> deleteTask(int id) async {
+    await localDb.deleteTask(id);
   }
 }
