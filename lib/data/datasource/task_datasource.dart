f170b77e2c6329e2d2086dfe184a1e51b74d5248
abdelrahman <rain.forest5023@gmail.com>
2025-12-19 00:38:01 +0200

    remove old sqflite db import

--- lib/data/datasource/task_datasource.dart
+++ lib/data/datasource/task_datasource.dart
@@ -1,24 +1,13 @@
 import 'dart:convert';
 
-import 'package:bexel_task/data/local/task_local_db.dart';
 import 'package:bexel_task/data/model/task_model.dart';
 import 'package:flutter/services.dart';
 
 abstract class TaskDatasource {
-  Future<List<TaskModel>> loadTasks();
   Future<List<TaskModel>> loadFromFiles();
 }
 
 class TaskDataSourceImp extends TaskDatasource {
-  final TaskLocalDb localDb;
-
-  TaskDataSourceImp(this.localDb);
-
-  @override
-  Future<List<TaskModel>> loadTasks() async {
-    return localDb.fetchAll();
-  }
-
   @override
   Future<List<TaskModel>> loadFromFiles() async {
     final rawData = await rootBundle.loadString('assets/data/data.json');
